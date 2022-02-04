f8d693acec454878f6788da8fdb673e01d6733f9
Alex Ross <alxrg@me.com>
2022-02-04 12:45:59 -0600

    Add_count in dictogram working

--- Code/app.py
+++ Code/app.py
@@ -1,20 +1,21 @@
 """Main script, uses other modules to generate sentences."""
 from flask import Flask
-
+#from histogram import histogram
 
 app = Flask(__name__)
-
 
 @app.before_first_request
 def before_first_request():
     """Runs only once at Flask startup"""
+    #histo = histogram()
+    #return histo
     # TODO: Initialize your histogram, hash table, or markov chain here.
 
 
 @app.route("/")
 def home():
     """Route that returns a web page containing the generated text."""
-    return "<p>TODO: Return a word here!</p>"
+    return f"<p>Random Word from Space.. Return a word here!</p>"
 
 
 if __name__ == "__main__":
